f287acccc6128dec5fb70bbde236c6f1a7ed04dd
sharozecodes <126905674+sharozecodes@users.noreply.github.com>
2023-09-19 17:01:38 -0400

    complete lab

--- server/app.py
+++ server/app.py
@@ -18,6 +18,15 @@ db.init_app(app)
 
 api = Api(app)
 
+
+@app.before_request
+def check_if_logged_in():
+    #if not logged in and endpoint is member related, reject user
+    if not session.get('user_id') \
+        and request.endpoint in ['member_index', 'member_article']:
+        return {'error': 'Unauthorized'}, 401
+    
+
 class ClearSession(Resource):
 
     def delete(self):
@@ -77,7 +86,7 @@ class CheckSession(Resource):
 
     def get(self):
         
-        user_id = session['user_id']
+        user_id = session.get('user_id')
         if user_id:
             user = User.query.filter(User.id == user_id).first()
             return user.to_dict(), 200
@@ -87,12 +96,19 @@ class CheckSession(Resource):
 class MemberOnlyIndex(Resource):
     
     def get(self):
-        pass
+        articles = [article.to_dict() for article in Article.query.filter(Article.is_member_only == True).all()]
+        
+        return make_response(jsonify(articles), 200) 
 
 class MemberOnlyArticle(Resource):
     
     def get(self, id):
-        pass
+        article = Article.query.filter(Article.id == id).first()
+        if article:
+            return make_response(jsonify(article.to_dict()), 200)
+        else:
+            return {}, 404
+        
 
 api.add_resource(ClearSession, '/clear', endpoint='clear')
 api.add_resource(IndexArticle, '/articles', endpoint='article_list')
